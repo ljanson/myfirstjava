ffbf62953e8f4ab6f316d7c5762bae01820780eb
ljanson <38383991+ljanson@users.noreply.github.com>
2018-04-14 16:18:32 -0700

    Update hellojava.java

--- hellojava.java
+++ hellojava.java
@@ -1,6 +1,6 @@
 public class hellojava {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
-      System.out.println("Hello2, World from github");
+      System.out.println("Hello3, World from github");
    }
 }
